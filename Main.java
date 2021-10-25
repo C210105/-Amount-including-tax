b65a200d53b75275e1c5e09795ec90e3946106ce
DANGLONG <c210105@ccg.ac.jp>
2021-10-26 03:35:25 +0900

    税込み金額を計算する-session 2

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
+/*
+    税込み金額を計算するプログラムです
+    割引：１５％
+    税込み：８％
+*/
+
 public class Main {
     public static void main(String[] args) {
         
         int price = 3500;
         System.out.println("定額：" + price + "円です。");
 
-        int discountPrice = price - ((15 * price) / 100);
+        int discountPrice = price - ((15 * price) / 100);//割引１５％を計算する
         System.out.println("割引価格：" + discountPrice + "円です。");
 
-        int amount = discountPrice + ((8 * discountPrice) / 100);
+        int amount = discountPrice + ((8 * discountPrice) / 100);//税込み８％計算する
         System.out.println("税込み金額:" + amount + "円です。");
     }
 }
